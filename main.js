831aa72ed80d7f4e2f527b41d5fa3d7cb8851a3e
Vivek Kesarwani <vivekkesarwani2005@gmail.com>
2025-01-03 00:50:06 +0530

    Added UI & Flip Animation

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ const pairs = items.flatMap(card => [card, card]);
 const pointsEl = document.getElementById("points");
 const movesEl = document.getElementById("moves");
 
+let points = 0;
+let totalMoves = 0;
+let move = 0;
+let cardsClicked = [];
+const maxMove = 2;
+
 function shuffleArray(array) {
     for(let i = array.length-1 ; i > 0 ; i--) {
         const k = Math.floor(Math.random() * (i+1));
@@ -15,33 +21,23 @@ function shuffleArray(array) {
 function GameInit()
 {
     const shuffledPairs = shuffleArray((pairs));
-
     cards.forEach((card,index) => {
         card.innerHTML = `<p>${shuffledPairs[index]}</p>`
     });  
 }
-GameInit();
-
-let points = 0;
-let totalMoves = 0;
-let move = 0;
-let cardsClicked = [];
-const maxMove = 2;
 
 function CardClicked(card){
     totalMoves++;
     movesEl.textContent = `Moves : ${totalMoves}`;
-    const p = card.firstElementChild
+    const p = card.firstElementChild;
     if(move < maxMove) {
         p.style.visibility = "visible";
+        card.setAttribute("id","flip");
         cardsClicked.push(p);
         move++;
     } 
     if(move === 2) {
         document.body.style.pointerEvents = "none";
-        console.log(cardsClicked[0].textContent);
-        console.log(cardsClicked[1].textContent);    
-
         if(cardsClicked[0].textContent === cardsClicked[1].textContent) {
             points++;
             pointsEl.textContent = `Points : ${points}`;
@@ -50,8 +46,9 @@ function CardClicked(card){
             },1000);
         } else {
             setTimeout(()=> {
-                cardsClicked.forEach((card) => {
-                    card.style.visibility = "hidden";
+                cardsClicked.forEach((cardClicked) => {
+                    cardClicked.style.visibility = "hidden";
+                    card.removeAttribute("id");
                 }); 
                 resetState();
             },2000);                            
@@ -70,3 +67,5 @@ cards.forEach((card) => {
         CardClicked(card)
     }
 )});
+
+GameInit();
